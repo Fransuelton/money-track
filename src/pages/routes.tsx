21d2a13292c25109c967a3d40251f982a3c4d70d
Fransuelton <elton6103@gmail.com>
2024-02-12 15:28:26 -0300

    feat: Add routes and page components

---
+++ src/pages/routes.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes,useLocation } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About/About";
+import { Dashboard } from "./Dashboard/Dashboard";
+
+const AppRoutes = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/saiba-mais" element={<AboutPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+function AboutPage() {
+  useDocumentTitle('Sobre');
+  return <About />;
+}
+function HomePage() {
+  useDocumentTitle('Home');
+  return <Home />;
+}
+function DashboardPage() {
+  useDocumentTitle('Dashboard');
+  return <Dashboard />;
+}
+
+function useDocumentTitle(title: string) {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = `${title} | MoneyTrack - Cofrinho Virtual`;
+  }, [location, title]);
+}
+
+export { AppRoutes };
