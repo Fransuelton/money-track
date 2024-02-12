f8e3027e579d9cd733e057f07fafbf76ea2aa152
Fransuelton <elton6103@gmail.com>
2024-02-12 15:31:41 -0300

    feat: Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+interface IButton {
+  content: string;
+  className?: string;
+  onClick?: () => void;
+  type?: "submit" | "button";
+  disabled?: boolean;
+}
+
+const Button = ({ content, className, onClick }: IButton) => {
+  return <StyledButton className={className} onClick={onClick}>{content}</StyledButton>;
+};
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  font-size: 1.8rem;
+  padding: 1.6rem 3.2rem;
+  justify-content: center;
+  border-radius: 1rem;
+  align-items: center;
+  transition: 0.2s;
+  border: none;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  @media (max-width: 480px){
+    font-size: 1.6rem;
+    padding: 1.2rem 2.4rem;
+  }
+`;
+
+export { Button };
