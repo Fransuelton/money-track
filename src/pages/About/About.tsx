ca692da75d1976773a924530367d1fb51b6e1666
Fransuelton <elton6103@gmail.com>
2024-02-15 16:50:53 -0300

    refactor: Refactor About page and Form component

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,6 @@
+import { styled } from "styled-components";
+
 import { Header, Footer } from "../../components/layout";
-import styled from "styled-components";
 import { Form } from "../../components";
 
 const About = () => {
@@ -7,42 +8,37 @@ const About = () => {
     <>
       <Header />
       <StyledSection>
-        <div>
-          <h2>O que é o MoneyTrack? 🤔</h2>
-          <p>
-            É uma aplicação web que transforma a arte de economizar em uma
-            jornada cativante e recompensadora. Com cofrinhos virtuais
-            personalizados e uma abordagem gamificada, nosso aplicativo não
-            apenas simplifica o controle financeiro, mas também transforma o ato
-            de poupar dinheiro em uma experiência divertida. Alcance seus
-            objetivos financeiros de forma mais eficiente, enquanto se diverte
-            no processo. Com o MoneyTrack, você molda seu caminho para o sucesso
-            financeiro, passo a passo.
-          </p>
+        <h2>O que é o MoneyTrack? 🤔</h2>
+        <p>
+          É uma aplicação web que transforma a arte de economizar em uma jornada
+          cativante e recompensadora. Com cofrinhos virtuais personalizados e
+          uma abordagem gamificada, nosso aplicativo não apenas simplifica o
+          controle financeiro, mas também transforma o ato de poupar dinheiro em
+          uma experiência divertida. Alcance seus objetivos financeiros de forma
+          mais eficiente, enquanto se diverte no processo. Com o MoneyTrack,
+          você molda seu caminho para o sucesso financeiro, passo a passo.
+        </p>
 
-          <h2>O Futuro 🚀</h2>
-          <p>
-            Está é a versão de MVP (Mínimo Produto Viável) onde estão
-            disponíveis as funcionalidades mais básicas como criar, excluir,
-            fazer depósitos e retiradas. Novas versões e funcionalidades
-            chegaram em breve de acordo com a evolução do projeto.
-          </p>
-        </div>
+        <h2>O Futuro 🚀</h2>
+        <p>
+          Está é a versão de MVP (Mínimo Produto Viável) onde estão disponíveis
+          as funcionalidades mais básicas como criar, excluir, fazer depósitos e
+          retiradas. Novas versões e funcionalidades chegaram em breve de acordo
+          com a evolução do projeto.
+        </p>
 
-        <div>
-          <h2>Compartilhe sua Opinião ✨</h2>
+        <h2>Compartilhe sua Opinião ✨</h2>
 
-          <p>
-            Queremos tornar o MoneyTrack ainda melhor e precisamos da sua ajuda.
-            Compartilhe seus comentários, sugestões e ideias para aprimorar sua
-            experiência com o aplicativo. Sua contribuição é fundamental para
-            moldar o futuro do MoneyTrack e garantir que atenda às suas
-            necessidades financeiras da melhor maneira possível. Agradecemos por
-            dedicar um momento para compartilhar sua valiosa opinião!
-          </p>
+        <p>
+          Queremos tornar o MoneyTrack ainda melhor e precisamos da sua ajuda.
+          Compartilhe seus comentários, sugestões e ideias para aprimorar sua
+          experiência com o aplicativo. Sua contribuição é fundamental para
+          moldar o futuro do MoneyTrack e garantir que atenda às suas
+          necessidades financeiras da melhor maneira possível. Agradecemos por
+          dedicar um momento para compartilhar sua valiosa opinião!
+        </p>
 
-          <Form />
-        </div>
+        <Form />
       </StyledSection>
       <Footer />
     </>
@@ -51,24 +47,18 @@ const About = () => {
 
 const StyledSection = styled.section`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  text-align: center;
   color: #fff;
-  margin-bottom: 1rem;
-
-  div {
-    width: 90rem;
-    padding: 2rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
-  }
+  text-align: center;
+  padding: 1rem;
+  gap: 2rem;
+  width: 91rem;
 
   h2 {
     font-size: 4rem;
+    margin-bottom: 0.5rem;
   }
 
   p {
@@ -76,8 +66,7 @@ const StyledSection = styled.section`
   }
 
   @media (max-width: 480px) {
-    flex-direction: column;
-
+    width: 100%;
     h2 {
       font-size: 2rem;
     }
@@ -85,9 +74,27 @@ const StyledSection = styled.section`
     p {
       font-size: 1.6rem;
     }
+  }
 
-    div {
-      width: 100%;
+  @media (min-width: 481px) and (max-width: 767px) {
+    width: 100%;
+    h2 {
+      font-size: 2.5rem;
+    }
+
+    p {
+      font-size: 1.6rem;
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 991px) {
+    width: 100%;
+    h2 {
+      font-size: 3rem;
+    }
+
+    p {
+      font-size: 1.6rem;
     }
   }
 `;
--- src/pages/About/Form.tsx
+++ src/pages/About/Form.tsx
@@ -4,32 +4,57 @@ import { Button } from "../../components/Button";
 
 const Form = () => {
   const [state, handleSubmit] = useForm("xyyrelar");
-  
+
   if (state.succeeded) {
     return <SucessMessage>Sua Mensagem foi enviada com sucesso!</SucessMessage>;
-  }
+  } else
+    return (
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledInput
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Digite seu nome aqui (ex: João da Silva)"
+        />
+        <StyledTextArea
+          id="message"
+          name="message"
+          placeholder="Deixe sua mensagem aqui (ex: Adorei o MoneyTrack, mas seria ótimo ter mais opções de personalização nos cofrinhos)"
+        />
 
-  return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledInput id="name" type="text" name="name" placeholder="Nome" />
-      <StyledTextArea id="message" name="message" placeholder="Mensagem" />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
 
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-
-      <Button type="submit" disabled={state.submitting} content="Enviar Mensagem"/>
-
-    </StyledForm>
-  );
+        <Button
+          type="submit"
+          disabled={state.submitting}
+          content="Enviar Mensagem"
+          className="form-button"
+        />
+      </StyledForm>
+    );
 };
 
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  width: 45rem;
+  width: 100%;
   gap: 1.5rem;
+  margin-top: 2rem;
 
-  @media (max-width: 480px) {
-    width: 35rem;
+  .form-button {
+    border: 0.1rem solid #fff;
+    color: #fff;
+    background: transparent;
+    transition: 0.2s;
+
+    &:hover {
+      background: #fff;
+      color: #009c4a;
+    }
   }
 `;
 
@@ -40,11 +65,11 @@ const StyledInput = styled.input`
   outline: none;
   background-color: #fff;
   border: none;
-  font-size: 1.7rem;
+  font-size: 1.6rem;
 
   &::placeholder {
-    color: #B3B3B3;
-    font-size: 1.7rem;
+    color: #b3b3b3;
+    font-size: 1.6rem;
   }
 `;
 
@@ -57,11 +82,11 @@ const StyledTextArea = styled.textarea`
   background-color: #fff;
   border: none;
   resize: none;
-  font-size: 1.7rem;
+  font-size: 1.6rem;
 
   &::placeholder {
-    color: #B3B3B3;
-    font-size: 1.7rem;
+    color: #b3b3b3;
+    font-size: 1.6rem;
   }
 `;
 
@@ -71,13 +96,7 @@ const SucessMessage = styled.p`
   text-align: center;
   width: 45rem;
   padding: 1rem;
-  background-color: #fff;
   border-radius: 0.8rem;
-
-
-  @media (max-width: 480px) {
-    font-size: 1.6rem;
-  }
 `;
 
 export { Form };
